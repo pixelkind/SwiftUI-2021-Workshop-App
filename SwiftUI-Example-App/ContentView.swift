2921bd3cd828c9e33c3de72b5229c3eec3460556
Garrit <garrit.schaap@ju.se>
2021-10-25 15:39:48 +0200

    Added State as an array for opening and closing cards

--- SwiftUI-Example-App/ContentView.swift
+++ SwiftUI-Example-App/ContentView.swift
@@ -8,14 +8,19 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var cardIsOpen = Array(repeating: false, count: 16)
+    
     var body: some View {
         NavigationView {
             VStack {
                 LazyVGrid(columns: [GridItem(.flexible()), GridItem(.flexible()), GridItem(.flexible()), GridItem(.flexible())], spacing: 10) {
                     
-                    ForEach(0..<16) { _ in
-                        Button(action: {}) {
-                            Image(systemName: "airplane")
+                    ForEach(0..<16) { index in
+                        Button(action: {
+                            cardIsOpen[index].toggle()
+                        }) {
+                            Image(systemName: cardIsOpen[index] ? "airplane" : "giftcard")
+
                         }
                         .font(.largeTitle)
                         .foregroundColor(.white)
