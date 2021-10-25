76f4dcfc899ff4ef236b53d3d51d6b84cc0422cb
Garrit <garrit.schaap@ju.se>
2021-10-25 15:01:41 +0200

    Initial Commit

---
+++ SwiftUI-Example-App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-Example-App
+//
+//  Created by Garrit Schaap on 2021-10-25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI-Example-App/SwiftUI_Example_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Example_AppApp.swift
+//  SwiftUI-Example-App
+//
+//  Created by Garrit Schaap on 2021-10-25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Example_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
